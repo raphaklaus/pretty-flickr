2ed9b37dbbfec7538b249f8499fd33704ad8f9ab
Raphael Klaus <raphaklaus@gmail.com>
2018-09-12 18:15:49 -0300

    feat(enum): add image size enums accordingly with Flickr API

---
+++ server/src/enums/image-sizes.enum.ts
@@ -0,0 +1,11 @@
+export enum ImageSizes {
+  Square = 'Square',
+  LargeSquare = 'Large Square',
+  Thumbnail = 'Thumbnail',
+  Small = 'Small',
+  Small320 = 'Small 320',
+  Medium = 'Medium',
+  Medium640 = 'Medium 640',
+  Medium800 = 'Medium 800',
+  Large = 'Large',
+}
