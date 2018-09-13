63220341d643d3bd9a826ba71e4a97db1fb34d9b
Raphael Klaus <raphaklaus@gmail.com>
2018-09-12 22:13:10 -0300

    feat(modal): add data property

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -1,20 +1,22 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React, { Component } from 'react'
+import { PhotoGallery } from '../../../core/interfaces/photo-gallery.interface'
 import { ModalClose } from './ModalClose'
 import { ModalContainer } from './ModalContainer'
 import { ModalContent } from './ModalContent'
 import { ModalImage } from './ModalImage'
 
-export default class Modal extends Component<{ src?: string, isOpened: boolean, handleClose: any}, {} > {
+export default class Modal extends Component<{ isOpened: boolean, handleClose: any,
+  data: PhotoGallery}, {} > {
   render() {
     return <ModalContainer isOpened={this.props.isOpened}>
       <ModalClose onClick={this.props.handleClose}>
         <FontAwesomeIcon icon={faTimes} />
       </ModalClose>
       <ModalContent>
-        <ModalImage src={this.props.src} />
-        <p>Usuário blaaah</p>
+        <ModalImage src={this.props.data.urls.original} />
+        <a href={this.props.data.link}>{this.props.data.owner}</a>
       </ModalContent>
     </ModalContainer>
   }
